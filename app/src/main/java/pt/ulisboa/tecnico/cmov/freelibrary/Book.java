35162831a1f0df6af51edbda2e77bfbdf1c590f1
lucas-mrq <lucasmarquet63800@gmail.com>
2023-05-26 20:05:53 +0100

    Delete isbn on books

--- app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/Book.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/Book.java
@@ -4,9 +4,8 @@ public class Book {
     private int id;
     private String title;
     private String author;
-    private String isbn;
 
-    public Book(int id, String title, String author, String isbn) {
+    public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
