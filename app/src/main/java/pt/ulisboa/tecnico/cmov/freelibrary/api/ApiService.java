678b28dda059a5d7dedda1b4a2177c7cc46fa93c
Evandro Giovanini <efgiovanini@gmail.com>
2023-06-10 01:09:32 +0200

    Get list of available books in library

--- app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/api/ApiService.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/api/ApiService.java
@@ -17,6 +17,9 @@ public interface ApiService {
     @GET("api/libraries/{id}")
     Call<Library> getLibraryById(@Path("id") int id);
 
+    @GET("api/libraries/books/{bookId}")
+    Call<List<Library>> getAvailableBooksInLibrary(@Path("bookId") int bookId);
+
     @PUT("api/libraries/{id}")
     Call<Library> updateLibrary(@Path("id") int id, @Body Library library);
 
