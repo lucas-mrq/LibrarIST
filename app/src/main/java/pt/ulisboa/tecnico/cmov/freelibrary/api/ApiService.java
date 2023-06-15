71eb796e2e63b1142feb9a67f98f440aca4cf2bf
Evandro Giovanini <efgiovanini@gmail.com>
2023-06-15 23:25:19 +0100

    Create new library

--- app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/api/ApiService.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/api/ApiService.java
@@ -9,6 +9,23 @@ import retrofit2.Call;
 import retrofit2.http.*;
 
 public interface ApiService {
+    @GET("/api/libraries")
+    Call<List<Library>> getAllLibraries();
+
+    @GET("api/libraries/{id}")
+    Call<Library> getLibraryById(@Path("id") int id);
+
+    @Headers("Content-Type: application/json")
+    @POST("/api/libraries")
+    Call<Library> createLibrary(@Body Library library);
+
+    @PUT("api/libraries/{id}")
+    Call<Library> updateLibrary(@Path("id") int id, @Body Library library);
+
+
+    @DELETE("api/libraries/{id}")
+    Call<Void> deleteLibrary(@Path("id") int id);
+
     @GET("api/books/{id}")
     Call<Book> getBookById(@Path("id") int id);
 
@@ -18,20 +35,8 @@ public interface ApiService {
     @GET("api/books/library/{id}")
     Call<List<Book>> getBooksByLibraryId(@Path("id") int id);
 
-    @GET("api/libraries/{id}")
-    Call<Library> getLibraryById(@Path("id") int id);
-
     @GET("api/libraries/books/{bookId}")
     Call<List<Library>> getAvailableBooksInLibrary(@Path("bookId") int bookId);
-
-    @PUT("api/libraries/{id}")
-    Call<Library> updateLibrary(@Path("id") int id, @Body Library library);
-
-    @DELETE("api/libraries/{id}")
-    Call<Void> deleteLibrary(@Path("id") int id);
-
-    @GET("/api/libraries")
-    Call<List<Library>> getAllLibraries();
 
     @Multipart
     @POST("/api/books")
