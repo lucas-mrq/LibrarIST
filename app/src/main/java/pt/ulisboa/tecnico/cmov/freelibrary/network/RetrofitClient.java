25b2da948a101b07c01efc9a19cb1d9bdf95ebd5
Evandro Giovanini <efgiovanini@gmail.com>
2023-06-16 01:21:36 +0100

    Use https server

--- app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/network/RetrofitClient.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/freelibrary/network/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.92.147.81:8080/";
+    private static final String BASE_URL = "https://librarist.evandro.pt/";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
